52b402d4f42b95731fba677ac73c0239dc3efc92
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-03-27 19:58:45 -0700

    added require('https') back

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
+const https = require('https')
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000
