8c3d80ff14d1db6305aef54de0771b0661a31dc9
ellenhc <59638391+ellenhc@users.noreply.github.com>
2021-03-27 19:29:23 -0700

    removed old files

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const https = require('https')
 require('dotenv').config();
 
 const usersController = require("./controllers/usersController.js");
+const { Connection } = require('pg');
 
 const PORT = process.env.PORT || 5000
 
@@ -15,10 +16,24 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.post('/api/getQuestions', handleQuestions);
-app.get("/search", usersController.search);
+app.post("/auth", handleLogin);
+app.get('/home', function(request, response) {
+    if (request.session.loggedin) {
+        response.send('Welcome back, ' + request.session.userName + '!');
+    } else {
+        response.send('Please login to view this page!');
+    }
+    response.end();
+});
 app.get('/', (req, res) => res.render('pages/index'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
@@ -62,5 +77,25 @@ function storeQuestions(questions) {
     for (let i = 0; i < questions.length; i++) {
         let question = { question: questions[i].question, correct_answer: questions[i].correct_answer, incorrect_answers: questions[i].incorrect_answers }; //just saves the question, the correct answer, and the incorrect answers and not the other data
         questionList.push(question); //adds it to the questionList array
+    }
+}
+
+function handleLogin(req, res) {
+    let userName = req.body.userName;
+    let userPassword = req.body.userPassword;
+    if (username && password) {
+        Connection.query('SELECT * FROM users WHERE "userName" = ? AND "userPassword" = ?', [userName, userPassword], function(error, results, fields) {
+            if (results.length > 0) {
+                request.session.loggedin = true;
+                request.session.userName = userName;
+                response.redirect('/home');
+            } else {
+                res.send('Incorrect Username and/or Password!');
+            }
+            res.end();
+        });
+    } else {
+        response.send('Please enter Username and Password!');
+        response.end();
     }
 }
